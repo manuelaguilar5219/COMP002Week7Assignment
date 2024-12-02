f4358e81cc9fb5d75c1c0efd78ed9a3f8ac6e0de
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2024-12-01 22:01:22 -0800

    Answered all questions

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("I think something that aided me was being comfortable with those around me. I was able to ask questions to others and I think that was helpful.");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("I wouldn't say that I would change anything specifically but it did go by rather quick. Yes I know that it was an 8 week course but maybe I would've preferred a slightly longer class.");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I'm gonna say no. The only reason I say that is because I had never worked with JavaScript so I didn't really know what to expect when it came to the course and what I would learn,");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("Everything in this class was new to me. I had zero experience with JavaScript");
